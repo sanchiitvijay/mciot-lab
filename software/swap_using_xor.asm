131c07ded21ad8730a0db9af235aa44a04443c32
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-07-02 19:39:51 +0530

    Create swap_using_xor.asm

---
+++ software/swap_using_xor.asm
@@ -0,0 +1,14 @@
+PRESERVE8	
+	  THUMB
+	area |.text|,CODE,READONLY
+  EXPORT __main
+
+__main
+	LDR r0,=0xF631024C
+	LDR r1,=0x17539ABD
+	EORS r0,r0,r1
+	EORS r1,r0,r1
+	EORS r1,r0,r0
+	EORS r0,r0,r1
+stop B stop
+	END
