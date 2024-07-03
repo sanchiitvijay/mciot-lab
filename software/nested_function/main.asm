3754b463d31fc9f48bc8eb6094cfe7e240faa6a4
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-07-03 22:08:41 +0530

    Update main.asm

--- software/nested_function/main.asm
+++ software/nested_function/main.asm
@@ -1,13 +1,11 @@
-PRESERVE8 ; Indicate the code here preserve  
-; 8 byte stack alignment         
-                     THUMB     ; Indicate THUMB code is used       
-                 AREA    |.text|, CODE, READONLY
-			   
-                                          EXPORT __main 
-			  EXTERN  func
-; Start of CODE area 
+   	PRESERVE8
+	THUMB
+	AREA |.text|, CODE, READONLY
+	EXPORT __main
+	EXTERN func
+
 __main
-	LDR r0,=0x10;
- BL func
+	LDR r0,=0x10
+	BL func
 stop B stop
-  END
+	END
