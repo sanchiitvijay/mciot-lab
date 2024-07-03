3ab9872270778caa30614b49d2f3b6154c34d591
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-07-03 19:52:29 +0530

    Update factorial.asm

--- software/factorial.asm
+++ software/factorial.asm
@@ -1,18 +1,17 @@
-             PRESERVE8 ; Indicate the code here preserve  
-; 8 byte stack alignment         
-                     THUMB     ; Indicate THUMB code is used       
-                 AREA    |.text|, CODE, READONLY		   
-              EXPORT __main			 
-; Start of CODE area 
+   	PRESERVE8
+	THUMB
+	AREA |.text|, CODE, READONLY
+	EXPORT __main
+	
 __main
-       movs r2,#1
-       movs r0,#5
-	   cmp r0,#0
-	   beq result
-	   movs r1,r0
-loop   muls r2,r1,r2
-       subs r1,r1,#1
-	   cmp r1,#0
-	   bgt loop
+	movs r2,#1
+	movs r1,#5
+loop   
+	muls r2,r1,r2
+	subs r1,r1,#1
+	cmp r1,#0
+	bgt loop
 result b result
        END
+	
+	
