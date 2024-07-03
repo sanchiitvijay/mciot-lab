d8ce6bfb4eeb7e84b3a711245192c40968e55c39
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-07-03 21:30:29 +0530

    Update clear_and_extract.asm

--- software/clear_and_extract.asm
+++ software/clear_and_extract.asm
@@ -1,24 +1,24 @@
-PRESERVE8 ; Indicate the code here preserve  
-; 8 byte stack alignment         
-                     THUMB     ; Indicate THUMB code is used       
-                 AREA    |.text|, CODE, READONLY
-			   
-              EXPORT __main			 
-; Start of CODE area
-
-
+   	PRESERVE8
+	THUMB
+	AREA |.text|, CODE, READONLY
+	EXPORT __main
+	
 __main
-   ;extracting
-	LDR r0,=0xFFC0FFFF
-	LSLS r0,r0,#(32-16-8)//(32-W-P)
-	LSRS r0,r0,#(32-8)
+	;extraction
+    ldr r0,=0x35968039
+	lsls r0,r0,#(32-16-8)
+	lsrs r0,r0,#(32-8)
+	
 	;clearing
-	LDR r0,=0xFFC0FFFF
-	MOVS r1,#16
-	MOVS R2,#08;(32-16-08)//( 32-W-P)
-	MOVS r3,#08
-	RORS r0,r0,r1
-	LSRS r0,r0,r2
-	RORS r0,r0,r3
-stop B stop
-  END
+	ldr r0,=0x04844634
+	movs r1,#16
+	movs r2,#8
+	movs r3,#8
+	
+	rors r0,r0,r1
+	lsrs r0,r0,r2
+	rors r0,r0,r3
+stop b stop
+	END
+	
+	
