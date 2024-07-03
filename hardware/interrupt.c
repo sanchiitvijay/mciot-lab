67bfaa60b5a4420d5bc21fba3563527ba3b1e673
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-07-03 22:28:46 +0530

    Update interrupt.c

--- hardware/interrupt.c
+++ hardware/interrupt.c
@@ -2,45 +2,44 @@
 #include "NUC1xx.h"
 #include "Driver/DrvSYS.h"
 #include "Driver/DrvGPIO.h"
-#include "NUC1xx-LB 002/Ascii.h"
+#include "NUC1xx-LB_002/Ascii.h"
 #include "NUC1xx-LB_002/LCD_Driver.h"
 
-void EINT1_Callback(){
-	int i;
-	DrvGPIO_Open(E_GPA,12,E_10_OUTPUT);
-	DrvGPIO_SetBit(E_GPA,12);
-	while(1){
-		i=12;
-		while(1<<14){
-			DrvGPIO_CLrBit(E_GPA,1);
-			DrvSYS Delay(10000);
-			DrvGPIO_SetBit(E GPA,i);
-			drvSYS Delay(10000);
-			1++;
-		}
-	}
-}
-vold EINTO_Callback(){
-	//For LCD.
-	Initial panel();
-	clr all panel();
-	print_lcd(0, "Hai!");
-	print lcd(1, "ENTO - ON");
-	//End of LCD
-	//For LED
+void EINT1_Callback(void) {
+    int i;
+    DrvGPIO_Open(E_GPA, 12, E_IO_OUTPUT);
+    DrvGPIO_SetBit(E_GPA, 12);
+    while(1) {
+        i = 12;
+        while(i < 14) {
+            DrvGPIO_ClrBit(E_GPA, 12);
+            DrvSYS_Delay(10000);
+            DrvGPIO_SetBit(E_GPA, 12);
+            DrvSYS_Delay(10000);
+            i++;
+        }
+    }
 }
 
+void EINT0_Callback(void) {
+    Initial_panel();
+    clr_all_panel();
+    print_lcd(0, "Hai!");
+    print_lcd(1, "EINT0 - ON");
+}
 
-int main(void){
-	UNLOCKREG();
-	DrvSYS Open (48000000);
-	LOCKREG();
+int main(void) {
+    UNLOCKREG();
+    DrvSYS_Open(48000000);
+    LOCKREG();
 
-	DrvGPIO_Open(E GPB,15,E_IO_INPUT);	
-	DrvGPIO_Open(E GPB,14,E_10_INPUT);
+    DrvGPIO_Open(E_GPB, 15, E_IO_INPUT);
+    DrvGPIO_Open(E_GPB, 14, E_IO_INPUT);
 
-	DrvGPIO_EnableEINT1(E GPB,15,EINT1 Callback());
-	DrvGPIO_ENableEINTO(E GPB,14,EINTO Callback());
-	while(1){}
+    DrvGPIO_EnableEINT1(E_GPB, 15, EINT1_Callback);
+    DrvGPIO_EnableEINT0(E_GPB, 14, EINT0_Callback);
 
-return 0;
+    while(1) {}
+
+    return 0;
+}
