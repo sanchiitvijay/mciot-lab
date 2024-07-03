9a421fc3276e0103b14ef6b2b9c7da5ab31f7209
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-07-04 00:18:13 +0530

    Update seven_segment.c

--- hardware/seven_segment.c
+++ hardware/seven_segment.c
@@ -1,55 +1,31 @@
-//
-// Smpl_7seg : counting from 0 to 9999 and display on 7-segment LEDs
-//
 #include <stdio.h>																											 
 #include "NUC1xx.h"
-#include "Driver\DrvSYS.h"
 #include "Seven_Segment.h"
 #include "DrvGPIO.h"
 #include "DrvSYS.h"
 
-// display an integer on four 7-segment LEDs
-void seg_display(int16_t value)
-{
-  int8_t digit;
-	digit = value / 1000;
-	close_seven_segment();
-	show_seven_segment(3,digit);
-	DrvSYS_Delay(5000);
-			
-	value = value - digit * 1000;
-	digit = value / 100;
-	close_seven_segment();
-	show_seven_segment(2,digit);
-	DrvSYS_Delay(5000);
-
-	value = value - digit * 100;
-	digit = value / 10;
-	close_seven_segment();
-	show_seven_segment(1,digit);
-	DrvSYS_Delay(5000);
-
-	value = value - digit * 10;
-	digit = value;
-	close_seven_segment();
-	show_seven_segment(0,digit);
-	DrvSYS_Delay(5000);
+void seg_display(int16_t val){
+	int8_t digit;
+	for (int i = 1; i<5; i++){
+		digit = val % 10;
+		val = val/10;
+		close_seven_segment();
+		show_seven_segment(4,digit);
+		DrvSYS_Delay(10000);
+	}
 }
 
-int32_t main (void)
-{
-	char TEXT1[16];
-				   int val;
-				   val=0000;
-	UNLOCKREG();
-	DrvSYS_Open(48000000);
-	LOCKREG();			 	
-
- 	while(1) 
-	{	DrvSYS_Delay(500);	 
-	 val=val++ ;
+int32_t main(void){
+	int16_t val;
+	val = 0000;
 	
-	seg_display(val);			     // write 1 to clear the flag
-	 
+	UNLOCKREG();
+	DrvSYS_Open(180000);
+	LOCKREG();
+	
+	while(1){
+		DrvSYS_Delay(1800000);
+		val++;
+		seg_display(val);
 	}
 }
