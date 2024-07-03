a5bdfdca0f976bb0c51f6519e9012590bfb9ed9a
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-07-03 11:45:39 +0530

    Update interrupt.c

--- hardware/interrupt.c
+++ hardware/interrupt.c
@@ -1,35 +1,46 @@
-// Smpl_7seg_ADC7 : ADC7 to read and display on lcd
-//
-
-#include <stdio.h>																											 
+#include <stdio.h>
 #include "NUC1xx.h"
-#include "Driver\DrvSYS.h"
-#include "Seven_Segment.h"
-#include "DrvADC.h"
-#include "LCD_Driver.h"
+#include "Driver/DrvSYS.h"
+#include "Driver/DrvGPIO.h"
+#include "NUC1xx-LB 002/Ascii.h"
+#include "NUC1xx-LB_002/LCD_Driver.h"
 
-	 
- int32_t main (void)
-{  uint16_t value;  
-    char TEXT[16];			   
-	
-	UNLOCKREG();
-	SYSCLK->PWRCON.XTL12M_EN = 1; 	//Enable 12Mhz and set HCLK->12Mhz
-	SYSCLK->CLKSEL0.HCLK_S = 0;
-	LOCKREG();
-	Initial_panel();  // initialize LCD pannel
-  clr_all_panel();  // clear LCD panel 
-  print_lcd(0,"variable reistor");
-
-	DrvADC_Open(ADC_SINGLE_END,ADC_SINGLE_OP , 0x80,INTERNAL_HCLK , 1); 	
-	while(1)
-   {
-    DrvADC_StartConvert();   // start A/D conversion
-    while(DrvADC_IsConversionDone()==FALSE);
-    value = ADC->ADDR[7].RSLT & 0xFFF;    
-    
-    sprintf(TEXT,"Value: %d",value); // convert ADC0 value into text
-    print_lcd(1, TEXT); // output TEXT to LCD
-   
-    }
+void EINT1_Callback(){
+	int i;
+	DrvGPIO_Open(E_GPA,12,E_10_OUTPUT);
+	DrvGPIO_SetBit(E_GPA,12);
+	while(1){
+		i=12;
+		while(1<<14){
+			DrvGPIO_CLrBit(E_GPA,1);
+			DrvSYS Delay(10000);
+			DrvGPIO_SetBit(E GPA,i);
+			drvSYS Delay(10000);
+			1++;
+		}
+	}
 }
+vold EINTO_Callback(){
+	//For LCD.
+	Initial panel();
+	clr all panel();
+	print_lcd(0, "Hai!");
+	print lcd(1, "ENTO - ON");
+	//End of LCD
+	//For LED
+}
+
+
+int main(void){
+	UNLOCKREG();
+	DrvSYS Open (48000000);
+	LOCKREG();
+
+	DrvGPIO_Open(E GPB,15,E_IO_INPUT);	
+	DrvGPIO_Open(E GPB,14,E_10_INPUT);
+
+	DrvGPIO_EnableEINT1(E GPB,15,EINT1 Callback());
+	DrvGPIO_ENableEINTO(E GPB,14,EINTO Callback());
+	while(1){}
+
+return 0;
