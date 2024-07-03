d7a582949b4e3ac0a78031631b2168c69b6862a2
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-07-03 22:02:08 +0530

    Update copying_memory.asm

--- software/copying_memory.asm
+++ software/copying_memory.asm
@@ -1,27 +1,22 @@
-;swapping of one memory to another memory (copying)      
-
-
-		PRESERVE8 ; Indicate the code here preserve  
-; 8 byte stack alignment         
-                     THUMB     ; Indicate THUMB code is used       
-                 AREA    |.text|, CODE, READONLY
-			   
-              EXPORT __main			 
-; Start of CODE area 
-
- 
+   	PRESERVE8
+	THUMB
+	AREA |.text|, CODE, READONLY
+	EXPORT __main
+	
 __main 	
-	LDR r0,=0x20000000 ; Source address
-	LDR r1,=0x20000040 ; Destination address
-	LDR r2,=10; number of bytes to copy
+	LDR r0,=0x20000000
+	LDR r1,=0x20000040
+	LDR r2,=10
+
 copy_loop
-	LDRB r3, [r0] ; read 1 byte
+	LDRB r3, [r0]
 	LDRB r4, [r1]
 	STRB r3, [r1]
 	STRB r4, [r0]
-	ADDS r0, r0, #1 ; increment source pointer
-	ADDS r1, r1, #1 ; increment destination pointer
-	SUBS r2, r2, #1 ; decrement loop counter
-	BNE copy_loop ; loop until all data copied
+
+	ADDS r0, r0, #1
+	ADDS r1, r1, #1
+	SUBS r2, r2, #1
+	BNE copy_loop
 stop B stop
-	          END
+	END
